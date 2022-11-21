99e7ac3782ecfbf060b5bf69dd29ecf8ba429f1d
AnujSharma100 <117298925+AnujSharma100@users.noreply.github.com>
2022-11-21 23:38:55 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,24 @@
+
+#assignment 2 ques 1
+#using for loop and a sample list.
+# list=[(2,5),(1,2),(4,4),(2,3),(2,1)]
+# for j in range (1,len(list)):
+#     for i in range(len(list)-j):
+#         if list[i][1]>list[i+1][1]:
+#             temp=list[i]
+#             list[i]=list[i+1]
+#             list[i+1]=temp
+# for i in range(len(list)):
+#     print(list[i],end=' ')
+
+
+#assignment 2 ques 2
+
+dict={}
+for i in range(97,123):
+    dict[chr(i)]=i
+print(dict)
+
+
+
+
